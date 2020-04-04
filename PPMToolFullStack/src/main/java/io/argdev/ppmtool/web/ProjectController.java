3c8d8a91abc0eb37974c26cb5d3ecfca348705b7
Argetim Ramadani <argdev@outlook.com>
2020-04-04 15:23:14 +0200

    Project Object validation handling

--- PPMToolFullStack/src/main/java/io/argdev/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/argdev/ppmtool/web/ProjectController.java
@@ -4,10 +4,13 @@ import io.argdev.ppmtool.domain.Project;
 import io.argdev.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,7 +23,12 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
         Project project1 = this.projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
