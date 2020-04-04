385fc08fbf1d7148ad09e51d7828e3a22baa804c
Argetim Ramadani <argdev@outlook.com>
2020-04-04 15:59:44 +0200

    Error message formatting

--- PPMToolFullStack/src/main/java/io/argdev/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/argdev/ppmtool/web/ProjectController.java
@@ -5,12 +5,15 @@ import io.argdev.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
@@ -26,7 +29,9 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(result.getFieldErrors().stream().collect(
+                    Collectors.toMap(FieldError::getField, fieldError -> Optional.ofNullable(fieldError.getDefaultMessage()).orElse("Null"))
+            ), HttpStatus.BAD_REQUEST);
         }
 
         Project project1 = this.projectService.saveOrUpdateProject(project);
